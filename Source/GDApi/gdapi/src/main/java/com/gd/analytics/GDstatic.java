b57494d873a58d921729a7173701cb6e3d9aeede
emredemir <demiremrece@gmail.com>
2018-04-16 16:39:17 +0300

    game api url changed.

--- Source/GDApi/gdapi/src/main/java/com/gd/analytics/GDstatic.java
+++ Source/GDApi/gdapi/src/main/java/com/gd/analytics/GDstatic.java
@@ -2,8 +2,8 @@ package com.gd.analytics;
 
 class GDstatic {
 
-    protected static final String TEST_GAME_API_URL = "http://test.game.api.gamedistribution.com/game/get";
-    protected static final String GAME_API_URL = "https://game.api.gamedistribution.com/game/get";
+    protected static final String TEST_GAME_API_URL = "http://game-dev.api.gamedistribution.com/game/get";
+    protected static final String GAME_API_URL = "https://game-prod.api.gamedistribution.com/game/get";
     protected static final String TUNNL_TAG_URL = "https://ana.tunnl.com"; // /at?id={{game-id}}&pageurl={{bundle-name}}
     protected static final String version = "v1.0";
     protected static Boolean enable = false;
@@ -16,7 +16,5 @@ class GDstatic {
     protected static String adUnit = "ca-app-pub-3940256099942544/4411468910"; // currently it is test.
     protected static String testInterUnitId = "ca-app-pub-3940256099942544/4411468910";
     protected static String testBannerUnitId = "ca-app-pub-3940256099942544/6300978111";
-    protected static String cordovaAdxUnitId = "ca-mb-app-pub-5192618204358860/8119020012";
     protected static boolean testAds = false;
-    protected static String affiliateId;
 }
